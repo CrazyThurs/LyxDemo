51ce045f1c9a3230b806fed96e703d7491959fe8
CrazyThurs <125956106+CrazyThurs@users.noreply.github.com>
2023-02-21 16:41:56 +0800

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,10 @@
+
+public class Bank {
+
+	    String name = "Bank1";
+	    double money = 888123.32;
+	    public String getMess(){
+	        return name + "银行所剩余额为："+money+"元";
+	    }
+
+}
---
+++ Run.java
@@ -0,0 +1,8 @@
+
+public class Run {
+	 public static void main(String[] args) {
+  Bank b1 = new Bank();
+  // System.out.println(b1.getMess);
+  System.out.println(b1.getMess());
+}
+}
